873e63465907eede1caf4b67222a973e0ef5fdc1
janoscarfilip <og200029@gmail.com>
2024-08-05 01:20:18 +0200

    Changed from " to '

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        "text-color": "var(--text-color)",
-        "background-color": "var(--background-color)",
         'emerald': 'rgb(16 185 129)',
+        'text-color': 'var(--text-color)',
+        'background-color': 'var(--background-color)',
       },
         'emerald': '0 0 2em rgb(16 185 129)',
+      }
     },
   },
   plugins: [],
