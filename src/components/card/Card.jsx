1e0c1794a6736be715e3e2cbf2c051a7bacc173f
janoscarfilip <og200029@gmail.com>
2024-08-04 05:17:33 +0200

    Added cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Card(){
     return (
-        <div className='text-white'>T</div>
+        <div className='flex flex-col md:flex-row justify-center gap-4'>
+            <a href="https://github.com/a21oscgu/Examensarbete" target='_blank' className='text-emerald-500 border-emerald-500 border-2 rounded-md p-4 border-color'>
+                Laddningstider mellan JSON och XML på nyhetssidor ↗️
+            </a>
+            <a href="https://github.com/janoscarfilip/" target='_blank' className='text-red-500 border-red-500 border-2 rounded-md p-4 border-color'>
+                Travel ↗️
+            </a>
+        </div>
     );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar(){
         <nav className="flex flex-row justify-between text-white p-8">
             <p className="text-xl">Portfolio</p>
             <div className="flex flex-row gap-x-8 text-xl">
-                <a href="https://www.linkedin.com/in/oscar-gustavsson/" target='_blank'>LinkedIn↗️</a>
-                <a href="https://github.com/janoscarfilip" target='_blank'>GitHub↗️</a>
+                <a href="https://www.linkedin.com/in/oscar-gustavsson/" target='_blank'>LinkedIn ↗️</a>
+                <a href="https://github.com/janoscarfilip" target='_blank'>GitHub ↗️</a>
             </div>
         </nav>
     );
