f5779f7333fd6d9aed24e59bb145757b7882f0b2
janoscarfilip <og200029@gmail.com>
2024-08-04 04:32:44 +0200

    updated logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar(){
     return (
         <nav className="flex flex-row justify-between text-white p-8">
-            <h1>Portfolio</h1>
+            <p className="text-xl">Portfolio</p>
             <div className="flex flex-row gap-x-8">
                 <a href="https://www.linkedin.com/in/oscar-gustavsson/" target='_blank'>LinkedIn↗️</a>
                 <a href="https://github.com/janoscarfilip" target='_blank'>GitHub↗️</a>
