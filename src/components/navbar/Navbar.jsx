6c7dd3fe773014b9bd60dedf8fa41b5ed7ff25a4
janoscarfilip <og200029@gmail.com>
2024-08-05 01:08:34 +0200

    Added GitHub icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
             <p className="text-xl">Portfolio</p>
             <div className="flex flex-row gap-x-8 text-xl">
                 <a href="https://www.linkedin.com/in/oscar-gustavsson/" target='_blank'>LinkedIn ↗️</a>
-                <a href="https://github.com/janoscarfilip" target='_blank'>GitHub ↗️</a>
+                <a href="https://github.com/janoscarfilip" className='flex' target='_blank'><img src="/github-mark-white.png" className='w-10' alt="Github white logo."/><p>GitHub ↗️</p></a>
             </div>
         </nav>
     );
