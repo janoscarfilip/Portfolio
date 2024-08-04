0a43a1ceb5857f40f0e0a7c88fb51d60fc18e6b9
janoscarfilip <og200029@gmail.com>
2024-08-04 04:27:14 +0200

    added flex to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Navbar(){
     return (
-        <nav className="text-emerald-500">
-            Portfolio
+        <nav className="flex flex-row justify-between text-white">
+            <h1>Portfolio</h1>
+            <div className="flex flex-row gap-x-8">
+                <a href="https://www.linkedin.com/in/oscar-gustavsson/">LinkedIn</a>
+                <a href="https://github.com/janoscarfilip">GitHub</a>
+            </div>
         </nav>
     );
 };
