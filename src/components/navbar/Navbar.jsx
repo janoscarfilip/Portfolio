1eefaef2080e7ba082342928b819776bcfe606c5
janoscarfilip <og200029@gmail.com>
2024-08-04 04:30:21 +0200

    updated links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Navbar(){
     return (
-        <nav className="flex flex-row justify-between text-white">
+        <nav className="flex flex-row justify-between text-white p-8">
             <h1>Portfolio</h1>
             <div className="flex flex-row gap-x-8">
-                <a href="https://www.linkedin.com/in/oscar-gustavsson/">LinkedIn</a>
-                <a href="https://github.com/janoscarfilip">GitHub</a>
+                <a href="https://www.linkedin.com/in/oscar-gustavsson/" target='_blank'>LinkedIn↗️</a>
+                <a href="https://github.com/janoscarfilip" target='_blank'>GitHub↗️</a>
             </div>
         </nav>
     );
