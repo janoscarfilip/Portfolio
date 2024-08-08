ab8d155b7d73e7b5d001a2f1114eae0c05919850
janoscarfilip <og200029@gmail.com>
2024-08-08 11:49:10 +0200

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,9 @@ function Navbar(){
     return (
         <nav className="flex flex-row justify-between text-main-color p-8">
             <p className="text-xl">Portfolio</p>
-            <div className="flex flex-row gap-x-8 text-xl">
+            <div className="flex flex-row items-center gap-x-8 text-xl">
                 <a href="https://www.linkedin.com/in/oscar-gustavsson/" target='_blank'>LinkedIn ↗️</a>
-                <a href="https://github.com/janoscarfilip" className='flex' target='_blank'><img src="/github-mark-white.png" className='w-10' alt="Github white logo."/><p>GitHub ↗️</p></a>
+                <a href="https://github.com/janoscarfilip" className='flex items-center gap-x-2' target='_blank'><img src="/github-mark-white.png" className='w-10' alt="Github white logo."/><p>GitHub ↗️</p></a>
             </div>
         </nav>
     );
