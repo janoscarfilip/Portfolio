f3dfbb9ca670c3eba4aa5f40cc6174ae88d9b8b8
janoscarfilip <og200029@gmail.com>
2024-08-09 13:14:04 +0200

    Deleted redundant code

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,5 @@
 import Navbar from '../components/navbar/Navbar';
-import Card1 from '../components/card/Card1';
-import Card2 from '../components/card/Card2';
-import Card3 from '../components/card/Card3';
-import Card4 from '../components/card/Card4';
+import Card from '../components/card/Card';
 
 export default function Home() {
     return (
@@ -10,10 +7,7 @@ export default function Home() {
             <div id="home" className='relative min-h-screen bg-gradient-to-br from-[#020024] via-[#0f0e24] to-[#252525]'>
                 <Navbar/>
                 <div className='flex flex-col gap-16 p-4 items-center'>
-                    <Card1/>
-                    <Card2/>
-                    <Card3/>
-                    <Card4/>
+                    <Card/>
                 </div>
             </div>
         </>
